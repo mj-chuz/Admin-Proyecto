059b2aa715178ef7eaa29e5a5399cec6177b44d5
DavidXie10 <davidxieli@gmail.com>
2022-06-15 19:24:43 -0600

    Implementado el handler de los juegos

---
+++ Planetario-PI-IS-BD/Handlers/JuegosHandler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Planetario.Models;
+
+namespace Planetario.Handlers {
+  public class JuegosHandler {
+
+    private String _direccionArchivo = AppContext.BaseDirectory + "/Juegos/juegos.json";
+
+    public List<JuegoModel> CargarListaJuegos(String tipo) {
+      List<JuegoModel> juegos = new List<JuegoModel>();
+      JToken juegosJSON = JObject.Parse(System.IO.File.ReadAllText(_direccionArchivo))["Juegos"];
+      foreach(JToken juego in (juegosJSON as JArray)) {
+        JuegoModel juegoLeido = new JuegoModel(juego["Nombre"].ToString(), juego["Link"].ToString(),juego["Imagen"].ToString(),juego["Tipo"].ToString());
+        if(juegoLeido.Tipo == tipo)
+          juegos.Add(juegoLeido);
+      }
+      return juegos;
+    }
+
+  }
+}
