7cbdef7a04f14042bce361e5d4fe74597456a2e0
DavidXie10 <davidxieli@gmail.com>
2022-06-16 21:11:34 -0600

    Se incluyó el modelo y el handler de juegos en la solución

--- Planetario-PI-IS-BD/Handlers/JuegosHandler.cs
+++ Planetario-PI-IS-BD/Handlers/JuegosHandler.cs
@@ -1,19 +1,23 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using Planetario.Models;
 
-namespace Planetario.Handlers {
-  public class JuegosHandler {
+namespace Planetario.Handlers
+{
+  public class JuegosHandler
+  {
 
     private String _direccionArchivo = AppContext.BaseDirectory + "/Juegos/juegos.json";
 
-    public List<JuegoModel> CargarListaJuegos(String tipo) {
+    public List<JuegoModel> CargarListaJuegos(String tipo)
+    {
       List<JuegoModel> juegos = new List<JuegoModel>();
       JToken juegosJSON = JObject.Parse(System.IO.File.ReadAllText(_direccionArchivo))["Juegos"];
-      foreach(JToken juego in (juegosJSON as JArray)) {
-        JuegoModel juegoLeido = new JuegoModel(juego["Nombre"].ToString(), juego["Link"].ToString(),juego["Imagen"].ToString(),juego["Tipo"].ToString());
-        if(juegoLeido.Tipo == tipo)
+      foreach (JToken juego in (juegosJSON as JArray))
+      {
+        JuegoModel juegoLeido = new JuegoModel(juego["Nombre"].ToString(), juego["Link"].ToString(), juego["Imagen"].ToString(), juego["Tipo"].ToString());
+        if (juegoLeido.Tipo == tipo)
           juegos.Add(juegoLeido);
       }
       return juegos;
--- Planetario-PI-IS-BD/Models/JuegoModel.cs
+++ Planetario-PI-IS-BD/Models/JuegoModel.cs
@@ -1,11 +1,10 @@
-
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
-namespace Planetario.Models {
-  public class JuegoModel {
+namespace Planetario.Models
+{
+  public class JuegoModel
+  {
 
     public String Nombre { get; }
     public String Link { get; }
@@ -13,21 +12,24 @@ namespace Planetario.Models {
 
     public String Tipo { get; set; }
 
-    public JuegoModel(String nombre, String link, String direccionImagen, String tipo) {
+    public JuegoModel(String nombre, String link, String direccionImagen, String tipo)
+    {
       Nombre = nombre;
       Link = link;
       DireccionImagen = direccionImagen;
       Tipo = tipo;
     }
 
-    public override bool Equals(object obj) {
+    public override bool Equals(object obj)
+    {
       return obj is JuegoModel model &&
              Nombre == model.Nombre &&
              Link == model.Link &&
              DireccionImagen == model.DireccionImagen;
     }
 
-    public override int GetHashCode() {
+    public override int GetHashCode()
+    {
       int hashCode = -683104575;
       hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nombre);
       hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Link);
